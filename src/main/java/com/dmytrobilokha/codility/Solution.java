d1f6bdcf0adcfaf9432879c5def5e1a9592f7408
Dmytro Bilokha <dmytro@posteo.net>
2019-04-29 19:03:29 +0300

    Tested the template on the binary gap challenge

--- src/main/java/com/dmytrobilokha/codility/Solution.java
+++ src/main/java/com/dmytrobilokha/codility/Solution.java
@@ -1,5 +1,26 @@
 package com.dmytrobilokha.codility;
 
-public class Solution {
+class Solution {
+
+    public int solution(int input) {
+        int maximumGap = 0;
+        int currentGap = 0;
+        boolean previousBit = false;
+        boolean insideGap = false;
+        for (int shiftedN = input; shiftedN > 0; shiftedN = shiftedN >> 1) {
+            boolean currentBit = (shiftedN & 1) != 0;
+            boolean newInsideGap = !currentBit && insideGap || !currentBit && previousBit;
+            if (insideGap && !newInsideGap) {
+                maximumGap = Math.max(maximumGap, currentGap);
+                currentGap = 0;
+            }
+            if (newInsideGap) {
+                currentGap++;
+            }
+            insideGap = newInsideGap;
+            previousBit = currentBit;
+        }
+        return maximumGap;
+    }
 
 }
--- src/test/java/com/dmytrobilokha/codility/SolutionTest.java
+++ src/test/java/com/dmytrobilokha/codility/SolutionTest.java
@@ -1,8 +1,31 @@
 package com.dmytrobilokha.codility;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
 
 @Test(groups = "unit")
 public class SolutionTest {
+
+    private Solution solution = new Solution();
+
+    @DataProvider(name = "binary-provider")
+    public Object[][] dataProvider() {
+        return new Object[][]{
+                {0b1, 0}
+                , {0, 0}
+                , {0b101, 1}
+                , {0b111, 0}
+                , {0b10010001, 3}
+                , {0b1000010001, 4}
+                , {0b11001101, 2}
+        };
+    }
+
+    @Test(dataProvider = "binary-provider")
+    public void findsSolution(int input, int output) {
+        assertEquals(solution.solution(input), output);
+    }
 
 }
